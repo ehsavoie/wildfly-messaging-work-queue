b286a9af483c8aa10dbd4618df1d3546161d9356
Emmanuel Hugonnet <emmanuel.hugonnet+gitkraken@gmail.com>
2019-04-03 18:29:37 +0200

    Trying to deploy rar file

---
+++ applications.sh
@@ -0,0 +1,25 @@
+oc replace --force  -f  \
+https://raw.githubusercontent.com/jboss-container-images/jboss-amq-7-broker-openshift-image/72-1.3.GA/amq-broker-7-image-streams.yaml
+oc replace --force -f \
+https://raw.githubusercontent.com/jboss-container-images/jboss-amq-7-broker-openshift-image/72-1.3.GA/amq-broker-7-scaledown-controller-image-streams.yaml
+
+oc import-image amq-broker-72-openshift:1.3
+
+oc apply -f ./service.yaml
+
+
+
+oc new-app --template=wildfly-messaging-work-queue-frontend \
+  -p SOURCE_REPOSITORY_URL=https://github.com/ehsavoie/wildfly-messaging-work-queue \
+  -p SOURCE_REPOSITORY_REF=master \
+  -p SOURCE_REPOSITORY_DIR=frontend \
+  -p CONTEXT_DIR=frontend \
+  -p ARTIFACT_DIR=frontend/target
+
+
+oc new-app --template=wildfly-messaging-work-queue-worker \
+  -p SOURCE_REPOSITORY_URL=https://github.com/ehsavoie/wildfly-messaging-work-queue \
+  -p SOURCE_REPOSITORY_REF=master \
+  -p SOURCE_REPOSITORY_DIR=worker \
+  -p CONTEXT_DIR=worker \
+  -p ARTIFACT_DIR=worker/target
