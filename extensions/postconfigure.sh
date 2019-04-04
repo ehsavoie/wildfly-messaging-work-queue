0588bc45ab9e06864d3f2742a88fc803dbb21256
Emmanuel Hugonnet <emmanuel.hugonnet+gitkraken@gmail.com>
2019-04-04 10:49:13 +0200

    Adding environment stuff

---
+++ extensions/postconfigure.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -x
+echo "Executing configure-amq-rar.cli"
+$JBOSS_HOME/bin/jboss-cli.sh --file=$JBOSS_HOME/extensions/configure-amq-rar.cli
+echo "Copying rar"
+cp resource-adapter-1.0.0.rar $JBOSS_HOME/standalone/deployments/.
