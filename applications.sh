561a3b2803d03360bd099c6a6c3c6654a4e51c34
Emmanuel Hugonnet <emmanuel.hugonnet+gitkraken@gmail.com>
2019-04-04 15:36:28 +0200

    Fixing

--- applications.sh
+++ applications.sh
@@ -2,7 +2,7 @@ oc replace --force  -f  \
 https://raw.githubusercontent.com/jboss-container-images/jboss-amq-7-broker-openshift-image/72-1.3.GA/amq-broker-7-image-streams.yaml
 oc replace --force -f \
 https://raw.githubusercontent.com/jboss-container-images/jboss-amq-7-broker-openshift-image/72-1.3.GA/amq-broker-7-scaledown-controller-image-streams.yaml
-
+oc replace --force -n openshift -f https://raw.githubusercontent.com/openshift-s2i/s2i-wildfly/master/imagestreams/wildfly-centos7.json
 oc import-image amq-broker-72-openshift:1.3
 
 oc apply -f ./service.yaml
